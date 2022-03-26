a91cc7e3fee7357609b45d56f9630580d3cfec7a
karinabregu <karinabregu@icloud.com>
2022-03-26 15:11:05 -0500

    Wrote code for first class

--- src/day0/HelloWorld.java
+++ src/day0/HelloWorld.java
@@ -3,5 +3,10 @@ package day0;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+
+        // extra line 1
+        // extra line 2
+        // extra line 3
     }
 }
